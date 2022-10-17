dd78ff9d329c4c86c8f220bf96ce65d8ed094568
Bunmi <foluwaboonmy@gmail.com>
2022-10-17 14:13:59 -0700

    Created three models and registered them

--- djangoprojects/Song/songcrud/musicapp/models.py
+++ djangoprojects/Song/songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
+from django.db import models
+import datetime
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.date.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)    
